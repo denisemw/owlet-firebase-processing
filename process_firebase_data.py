a94797f50b242f545267148e27bda5c607eb038e
denisemw <dwerchan@gmail.com>
2024-08-08 13:15:20 -0400

    Update process_firebase_data.py

--- process_firebase_data.py
+++ process_firebase_data.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 
     os.chdir(cwd)
 
-    if "nest" in subject_name:
+    if "vpc" in subject_name:
         subprocess.call(['python3', 'nest-video-cropping.py', subject_name, str(subDate)])
         date2 = str(subDate).replace("/", "")
         date2 = date2.replace("-", "_")
@@ -59,36 +59,46 @@ if __name__ == '__main__':
     
     else:
 
-        # subprocess.call(['python3', 'video-cropping.py', subject_name, str(subDate)])
+        subprocess.call(['python3', 'video-cropping.py', subject_name, str(subDate)])
+        PATH_TO_DATA = '/Users/werchd01/Documents/ORCA_Subjects/'
+        PATH_TO_OWLET = "/Users/werchd01/Documents/GitHub/OWLET/"
+        PATH_TO_TASKS = "/Users/werchd01/Documents/GitHub/OWLET-preprocessing/ORCA_2.0_Tasks/"
 
-        subDir = Path('/Users/werchd01/Documents/ORCA_Subjects/' + subject_name)
-        os.chdir("/Users/werchd01/Documents/GitHub/OWLET/")
+        subDir = Path(PATH_TO_DATA + subject_name)
+        os.chdir(PATH_TO_OWLET)
         sub_id = str(subject_name).replace("orca_", "")
 
-        vid1 = "/Users/werchd01/Documents/ORCA_Subjects/" + subject_name + "/" + sub_id + "_JRAttention.mp4"
-        vid2 = "/Users/werchd01/Documents/ORCA_Subjects/" + subject_name  + "/" + sub_id + "_VPC.mp4"
-        vid3 = "/Users/werchd01/Documents/ORCA_Subjects/" + subject_name  + "/" + sub_id + "_ProceduralMemory.mp4"
-        vid4 = "/Users/werchd01/Documents/ORCA_Subjects/" + subject_name + "/" + sub_id  + "_Cecile.mp4"
-        vid5 = "/Users/werchd01/Documents/ORCA_Subjects/" + subject_name  + "/" + sub_id + "_RelationalMemory.mp4"
+        vid1 = PATH_TO_DATA + subject_name + "/" + sub_id + "_JRAttention.mp4"
+        vid2 = PATH_TO_DATA + subject_name  + "/" + sub_id + "_VPC.mp4"
+        vid3 = PATH_TO_DATA + subject_name  + "/" + sub_id + "_ProceduralMemory.mp4"
+        vid4 = PATH_TO_DATA + subject_name + "/" + sub_id  + "_Cecile.mp4"
+        vid5 = PATH_TO_DATA + subject_name  + "/" + sub_id + "_RelationalMemory.mp4"
+
+        owlet_command = PATH_TO_OWLET + 'OWLET.py'
+        jrattn_path = PATH_TO_TASKS + 'JRAttention'
+        vpc_path =  PATH_TO_TASKS + 'VPC'
+        proc_path =  PATH_TO_TASKS + 'ProceduralMemory'
+        cecile_path = PATH_TO_TASKS + 'Cecile'
+        rel_path =  PATH_TO_TASKS + 'RelationalMemory'
 
         try: 
-            subprocess.call(['python3', '/Users/werchd01/Documents/GitHub/OWLET/OWLET.py', "--subject_video", vid1, "--experiment_info", "/Users/werchd01/Documents/GitHub/OWLET-preprocessing/ORCA_2.0_Tasks/JRAttention", "--override_audio_matching"])
+            subprocess.call(['python3', owlet_command, "--subject_video", vid1, "--experiment_info", jrattn_path, "--override_audio_matching"])
         except:
             print("No JR Attention video")
         try:
-            subprocess.call(['python3', '/Users/werchd01/Documents/GitHub/OWLET/OWLET.py', "--subject_video", vid2, "--experiment_info", "/Users/werchd01/Documents/GitHub/OWLET-preprocessing/ORCA_2.0_Tasks/VPC", "--override_audio_matching"])
+            subprocess.call(['python3', owlet_command, "--subject_video", vid2, "--experiment_info", vpc_path, "--override_audio_matching"])
         except:
             print("No VPC video")
         try:
-            subprocess.call(['python3', '/Users/werchd01/Documents/GitHub/OWLET/OWLET.py', "--subject_video", vid3, "--experiment_info", "/Users/werchd01/Documents/GitHub/OWLET-preprocessing/ORCA_2.0_Tasks/ProceduralMemory", "--override_audio_matching"])
+            subprocess.call(['python3', owlet_command, "--subject_video", vid3, "--experiment_info", proc_path, "--override_audio_matching"])
         except:
             print("No Procedural Memory video")
         try:
-            subprocess.call(['python3', '/Users/werchd01/Documents/GitHub/OWLET/OWLET.py', "--subject_video", vid4, "--experiment_info", "/Users/werchd01/Documents/GitHub/OWLET-preprocessing/ORCA_2.0_Tasks/Cecile", "--override_audio_matching"])
+            subprocess.call(['python3', owlet_command, "--subject_video", vid4, "--experiment_info", cecile_path, "--override_audio_matching"])
         except:
             print("No Cecile video")
         try:
-            subprocess.call(['python3', '/Users/werchd01/Documents/GitHub/OWLET/OWLET.py', "--subject_video", vid5, "--experiment_info", "/Users/werchd01/Documents/GitHub/OWLET-preprocessing/ORCA_2.0_Tasks/RelationalMemory", "--override_audio_matching"])
+            subprocess.call(['python3', owlet_command, "--subject_video", vid5, "--experiment_info", rel_path, "--override_audio_matching"])
         except:
             print("No RelationalMemory video")
 
